177cf2fef7cb5f176843f81c24ec8233a8caca11
H4R335HR <121650077+H4R335HR@users.noreply.github.com>
2023-05-25 00:47:00 +0530

    Some more changes to js to make the code work everywhere

--- code.js
+++ code.js
@@ -1,9 +1,20 @@
 function reloadCaptcha() {
     var captchaImage = document.getElementById("imgCaptcha");
-    captchaImage.src = "captcha.jsp?" + new Date().getTime();
+    var imgBaseName = imgNameExtract(captchaImage.src);
+    captchaImage.src = imgBaseName +"?" + new Date().getTime();
 }
 const span = document.createElement("span");
 span.classList.add("info");
 const cont = "Q2FwdGNoYSBQcm9qZWN0IC0gRGV2ZWxvcGVkIGJ5IEhhcmVlc2ggUmFqZW5kcmFuIGZvciBUQ1MgaU9OIEN5YmVyc2VjdXJpdHkgSW50ZXJuc2hpcCAyMDIz";
 span.textContent = atob(cont);
 document.body.appendChild(span);
+
+function imgNameExtract(text) {
+    var regex = /^[^?]+/;
+    var matches = text.match(regex);
+    if (matches && matches.length > 0) {
+      return matches[0];
+    } else {
+      return text;
+    }
+  }
